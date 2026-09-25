09053b92894aeffe5a9afc753bb78c1ebf9d3125
JazminDzib <a22216871@alumnos.uady.mx>
2026-09-25 02:21:07 -0600

    feat: wire routes and error handler in Express app

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,17 @@ import  express, {Request, Response} from "express";
 import cors from  "cors";
 import { initDb } from "./database/sequelize";
 import "./models/index"; 
+import indexRoutes from "./routes/indexRoutes";
+import { errorHandle } from "./middleware/errorHandle";
+
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+app.use("/api", indexRoutes);
+app.use(errorHandle);
 
 
 const main = async() => {
